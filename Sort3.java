74e0b316810c2f2a14b7e95800b7cf696719258e
ponjagannath <ponjagannath@gmail.com>
2016-09-27 17:33:18 +0530

    Add files via upload

---
+++ Sort3.java
@@ -0,0 +1,23 @@
+//page 844
+//Collection method sort with a custom Coparator object
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+public class Sort3 {
+    public static void main(String[] args){
+        
+        List<Time2> list = new ArrayList<Time2>();
+        
+        list.add(new Time2(6,24,34));
+        list.add(new Time2(18,14,58));
+        list.add(new Time2(6,05,34));
+        list.add(new Time2(12,14,58));
+        list.add(new Time2(6,24,22));
+        
+        System.out.printf("Unsorted array elements:\n%s\n",list);
+        
+        Collections.sort(list, new TimeComparator());
+        
+        System.out.printf("Sorted array elements:\n%s\n",list);       
+    }
+}
