02fe7fe7d0df7a936d377c4d418bfbef955a72a0
ponjagannath <ponjagannath@gmail.com>
2016-09-28 18:58:37 +0530

    Add files via upload

---
+++ Algorithms2.java
@@ -0,0 +1,32 @@
+//page 851
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Algorithms2 {
+    public static void main(String[] args){
+        String[] colors = {"red", "white", "yellow", "blue"};
+        List<String> list1 = Arrays.asList(colors);
+        ArrayList<String> list2 = new ArrayList<String>();
+        
+        list2.add("black");
+        list2.add("red");
+        list2.add("green");
+        
+        System.out.println("Before addAll, list2 contains");
+        System.out.println(list2);
+        
+        Collections.addAll(list2, colors);
+        System.out.println("After addAll, list2 contains");
+        System.out.println(list2);
+        
+        int frequency = Collections.frequency(list2, "red");
+        System.out.printf("\nFrequency of red in list2 is: %d\n", frequency);
+        
+        boolean disjoint = Collections.disjoint(list1, list2);
+        System.out.printf("list1 and list2 %s elements in common\n", (disjoint ? "do not have" : "have"));
+            
+    }
+    
+}
